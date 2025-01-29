e84bc025c147486a93146e0cfa49f4849587ccb4
jrc <jrc@josericardocuadra.com>
2025-01-29 09:14:49 -0500

    Added commnet explaining issue

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import dishes from '../src/assets/data/dashboard/dishes.json'
 
 function App() {
   return (
+    // Bordered column not working as it should, does not drop the two boxes of Customer Adress Label and Customer Address Input onto the line below as it does to Lukas on the video //
       <Card title={'Order Title'} style={{margin: 20}}>
         <Descriptions bordered column={{lg: 1, md: 1, sm: 1}}>
           <Descriptions.Item label='Customer'>
