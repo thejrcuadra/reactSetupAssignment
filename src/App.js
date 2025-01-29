29e751c8c6f70acf28b6e6a461313204e590b504
jrc <jrc@josericardocuadra.com>
2025-01-29 09:10:13 -0500

    weird formatting issues not working -- bordered column

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import {Card, Descriptions, Divider, List, Button} from 'antd';
+import dishes from '../src/assets/data/dashboard/dishes.json'
+
 function App() {
   return (
-    <div>
-      <span>Hello!</span>
-    </div>
+      <Card title={'Order Title'} style={{margin: 20}}>
+        <Descriptions bordered column={{lg: 1, md: 1, sm: 1}}>
+          <Descriptions.Item label='Customer'>
+            Alejandro Redondo
+          </Descriptions.Item>
+          <Descriptions.Item label='Customer Address'>
+            Calle Pancho 34, Madrid, Spain
+          </Descriptions.Item>
+        </Descriptions>  
+        <Divider />
+      </Card>
   );
 }
 
