8ba4f4bdc7c8e1ca3f88693d19a527522b208f62
jrc <jrc@josericardocuadra.com>
2025-02-03 18:11:58 -0500

    React Router DOM -- Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
  import DetailedOrder from "./modules/DetailedOrder";
  import Orders from "./modules/Orders";
  import {Routes, Route} from 'react-router-dom';
- import { Layout, Image} from "antd";
+ import { Layout, Image} from 'antd';
 
  const {Sider, Content, Footer} = Layout;
 
@@ -9,13 +9,21 @@ function App() {
   return (
       <Layout>
         <Sider style={{height: "100vh", backgroundColor: "white"}}>
+          <Image src="https://logodix.com/logo/431265.png" preview={false}/>
         </Sider>
-
+        <Layout>
+          <Content>
+            <Routes>
+              <Route path="" element={<Orders />} />
+              <Route path="order/:id" element={<DetailedOrder />} />
+            </Routes>
+          </Content>
+          <Footer style={{textAlign: 'center'}}>
+            Uber Eats Restaurant Dashboard - Est. 2022
+          </Footer>
+        </Layout>
       </Layout>
-      // <Routes>
-      //   <Route path="" element={<Orders />} />
-      //   <Route path="order/:id" element={<DetailedOrder />} />
-      // </Routes>
+      // 
   );
 }
 
